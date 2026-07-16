7b445932608166d0cacfb6d9d2e9c47224cfa548
JojitaGamerHD <johaquinfernandez0@gmail.com>
2026-07-16 15:04:59 -0400

    Actualizacion JWT

--- src/main/java/com/example/homerodoh/security/JwtService.java
+++ src/main/java/com/example/homerodoh/security/JwtService.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class JwtService {
 
     private static final String SECRET =
-            "MiClaveSuperSecretaParaJWT2026HomeroDohProyecto";
+            "${JWT_SECRET}";
 
     private SecretKey getSigningKey() {
 
